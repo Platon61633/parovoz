65d6806e2cc9dbc94a141d87efcccb0af88607fa
Platon61633 <peredreevplaton@gmail.com>
2023-07-21 13:50:35 +0300

    +media

--- src/components/kontakts.jsx
+++ src/components/kontakts.jsx
@@ -7,13 +7,13 @@ const Kontakts = () => {
         <div id='kontakts'>
         <div className='item'>
             <a className='sfaw' href="https://instagram.com/parovoz_tgn?igshid=NTc4MTIwNjQ2YQ==">
-                <img src={insta} alt='insta' width={80}/>
+                <img src={insta} alt='insta'/>
                 <span>parovoz_tgn</span>
             </a>
             </div>
             <div className='item'>
                 <a  className='sfaw' href="https://wa.me/79897148911">
-                <img src={whats} alt='WhatsApp'  width={80}/>
+                <img src={whats} alt='WhatsApp'/>
                 <span>+7 989 714-89-11</span>
                 </a>
             </div>
